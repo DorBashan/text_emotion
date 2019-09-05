6287da5216f59d01ca2407a497f3e2de86569ed7
dorbashan <dorbashan@planckresolution.com>
2019-09-05 12:29:22 +0300

    video splitter

---
+++ main/models/srt_part.py
@@ -0,0 +1,32 @@
+
+class SrtPart:
+
+    def __init__(self):
+        self.index = -1
+        self.start = -1
+        self.end = -1
+        self.text = ''
+
+    def set_index(self, index):
+        self.index = index
+
+    def get_index(self):
+        return self.index
+
+    def set_start(self, start):
+        self.start = start
+
+    def get_start(self):
+        return self.start
+
+    def set_end(self, end):
+        self.end = end
+
+    def get_end(self):
+        return self.end
+
+    def set_text(self, text):
+        self.text = text
+
+    def get_text(self):
+        return self.text
