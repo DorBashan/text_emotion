a61a1c197c146339775f34470dffc2fc1623a822
dorbashan <dorbashan@planckresolution.com>
2019-09-05 15:23:45 +0300

    subtitles creator

--- logic/subtitles_creator.py
+++ logic/subtitles_creator.py
@@ -3,14 +3,63 @@ from typing import List
 from models.colored_srt_part import ColoredSrtPart
 from shutil import copyfile
 
+from models.srt_part import SrtPart
+
 
 class SubtitlesCreator:
 
-    def create(self, input_file_path, output_file_path, parts: List[ColoredSrtPart]):
+    @staticmethod
+    def create(input_file_path, output_file_path, parts: List[ColoredSrtPart]):
 
         copyfile(input_file_path, output_file_path)
 
-        f = open(output_file_path, "a+")
+        f = open(input_file_path, "r")
+        contents = f.readlines()
+        f.close()
 
-        for part in parts:
-            f.write('test')
+        f = open(input_file_path, "r")
+        line_reader = f.readlines()
+
+        globals_index_to_colors = []
+        globals_index_to_close_font_symbol = []
+
+        current_index = 1
+        current_color = ''
+        global_index = 0
+        for line in line_reader:
+            if current_index == 1:
+                part = [x for x in parts if x.get_index() == int(line)][0]
+                current_color = part.get_color()
+            if current_index == 2:
+                globals_index_to_colors.append(tuple((global_index + 1, current_color)))
+            if line == '\n':
+                globals_index_to_close_font_symbol.append(global_index + 1)
+                current_index = 1
+                global_index = global_index + 1
+                continue
+
+            current_index = current_index + 1
+            global_index = global_index + 1
+
+        globals_index_to_close_font_symbol.append(global_index + 1)
+
+        f.close()
+
+        for index, color in globals_index_to_colors:
+            contents.insert(index, '<font color="{}">'.format(color))
+
+        for index in globals_index_to_close_font_symbol:
+            contents.insert(index, '</font>')
+
+        f = open(output_file_path, "w")
+        contents = "".join(contents)
+        f.write(contents)
+        f.close()
+
+
+if __name__ == '__main__':
+    part = SrtPart()
+    part.set_index(1)
+    l = [ColoredSrtPart(part, '#ff0000')]
+    SubtitlesCreator.create('pulp_test.srt', 'output.srt', l)
+
--- logic/subtitles_painter.py
+++ logic/subtitles_painter.py
@@ -3,6 +3,11 @@ from typing import Dict, List
 from models.colored_srt_part import ColoredSrtPart
 from models.srt_part import SrtPart
 
+emotions_to_colors = {
+    'happy': '#add8e6',
+    'sad': '#778899'
+}
+
 
 class SubtitlesPainter:
 
@@ -10,9 +15,9 @@ class SubtitlesPainter:
     def paint(parts_to_emotions: Dict[SrtPart, str]) -> List[ColoredSrtPart]:
         colored_parts = []
         for part, emotion in parts_to_emotions.items():
-            if emotion == 'happy':
-                colored_part = ColoredSrtPart(srt_part=part, color_hexa='#ffff00')
-                colored_parts.append(colored_part)
+            color_hexa = emotions_to_colors.get(emotion.lower(), '#000000')
+            colored_part = ColoredSrtPart(srt_part=part, color_hexa=color_hexa)
+            colored_parts.append(colored_part)
 
         return colored_parts
 
